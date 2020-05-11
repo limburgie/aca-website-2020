74e546001ceb984e3153f6ba087a81ad2ec7f7a1
Peter Mesotten <peter.mesotten@aca-it.be>
2020-05-11 22:26:18 +0200

    Added dato CMS content integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,20 @@ module.exports = {
 				pathToConfigModule: `src/utils/typography`,
 			},
 		},
+		{
+			resolve: `gatsby-source-datocms`,
+			options: {
+				// You can find your read-only API token under the Settings > API tokens
+				// section of your administrative area:
+				apiToken: `4a58b8091c641799785bae5128a86b`,
+
+				// If you are working on development/staging environment, you might want to
+				// preview the latest version of records instead of the published one:
+				previewMode: false,
+
+				// Disable automatic reloading of content when some change occurs on DatoCMS:
+				disableLiveReload: false,
+			},
+		},
 	]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
 import React from "react"
+import Img from 'gatsby-image'
+import {graphql} from "gatsby";
 import Layout from "../components/layout";
 
-export default function Home() {
+export default function Home({data}) {
 	return (
 		<Layout>
-			<h1>ACA IT-Solutions</h1>
+			<Img fixed={data.datoCmsHomepage.logo.fixed}/>
+			<p/>
+			<div dangerouslySetInnerHTML={{__html: data.datoCmsHomepage.intro}}/>
 		</Layout>
 	);
 }
+
+export const query = graphql`
+    query AboutQuery {
+        datoCmsHomepage {
+            logo {
+                fixed(width: 100) {
+                    ...GatsbyDatoCmsFixed
+                }
+            }
+			intro
+        }
+    }
+`
